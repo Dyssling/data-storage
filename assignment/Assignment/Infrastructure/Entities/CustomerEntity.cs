a6df229a36ee450eb8ef5ba20ca990acf16b213d
Dyssling <olle.lundgarden@hotmail.com>
2024-02-06 16:21:08 +0100

    Entities for CF created in code.

---
+++ assignment/Assignment/Infrastructure/Entities/CustomerEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities
+{
+    public class CustomerEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        public ICollection<OrderEntity> Orders { get; set; } = new HashSet<OrderEntity>();
+    }
+}
---
+++ assignment/Assignment/Infrastructure/Entities/OrderEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities
+{
+    public class OrderEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+        public CustomerEntity Customer { get; set; } = null!;
+
+        [Required]
+        public string ProductList { get; set; } = null!;
+
+        [Required]
+        public decimal Cost { get; set; }
+    }
+}
