58ac3f939d5ba65bbc8b85d98af6d86c93175c50
Dyssling <olle.lundgarden@hotmail.com>
2024-02-06 20:10:18 +0100

    Data context and dependency injection created. TODO: AddMigration

---
+++ assignment/Assignment/Infrastructure/Contexts/CustomersDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+    public class CustomersDataContext : DbContext
+    {
+        public CustomersDataContext(DbContextOptions<CustomersDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+    }
+}
--- assignment/Assignment/Presentation/Program.cs
+++ assignment/Assignment/Presentation/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder();
+builder.ConfigureServices(services =>
+{
+    services.AddDbContext<CustomersDataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Education\data-storage\assignment\Assignment\Infrastructure\Data\customers_and_orders_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
+});
+
+builder.Build();
