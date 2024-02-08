760d9f87574ce9e85c12a10953eccbb195a31993
Dyssling <olle.lundgarden@hotmail.com>
2024-02-08 12:42:16 +0100

    Database updated.

---
+++ assignment/Assignment/Infrastructure/Factories/CustomerDataContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Factories
+{
+    public class CustomerDataContextFactory : IDesignTimeDbContextFactory<CustomersDataContext>
+    {
+        public CustomersDataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CustomersDataContext>();
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Education\data-storage\assignment\Assignment\Infrastructure\Data\customers_and_orders_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+
+            return new CustomersDataContext(optionsBuilder.Options);
+        }
+    }
+}
