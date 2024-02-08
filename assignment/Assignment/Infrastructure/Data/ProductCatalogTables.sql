5523cd89f53384d6f39fea1083452f9d5c868f90
Dyssling <olle.lundgarden@hotmail.com>
2024-02-08 13:17:52 +0100

    ProductCatalog created. First tables designed.

---
+++ assignment/Assignment/Infrastructure/Data/ProductCatalogTables.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE Products (
+	ArticleNumber nvarchar(max) not null primary key,
+	[Name] nvarchar(max) not null,
+	[Description] nvarchar(max) null
+)
+
+CREATE TABLE Categories (
+	Id int identity not null primary key,
+	[Name] nvarchar(max) not null unique
+)
+
+CREATE TABLE Images (
+	Id int identity not null primary key,
+	ImageURL nvarchar(max) not null
+)
+
+CREATE TABLE Reviews (
+	Id int identity not null primary key,
+	CustomerId int not null,
+	Rating tinyint not null,
+	Title nvarchar(max) null,
+	Content nvarchar(max) null
+)
+
+CREATE TABLE ProductCategories (
+	ArticleNumber nvarchar(max) not null references Products(ArticleNumber),
+	CategoryId int not null references Categories(Id)
+)
