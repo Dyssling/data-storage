f3cb3261101a42885d539d3e07dd63ef8e4df772
Dyssling <olle.lundgarden@hotmail.com>
2024-02-10 13:27:36 +0100

    CreateAsync test created.

---
+++ assignment/Assignment/Infrastructure.Tests/Repositories.Tests/CategoryRepository_Tests.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tests.Repositories.Tests
+{
+    public class CategoryRepository_Tests
+    {
+        private readonly ProductsDataContext _context = new ProductsDataContext(new DbContextOptionsBuilder<ProductsDataContext>().UseInMemoryDatabase($"{Guid.NewGuid()}").Options); //Här skapar jag en ny instans av mitt context. Jag skickar in mina options, som jag får ifrån en ny OptionsBuilder som jag skapar, som gör att mitt context använder sig av en InMemoryDatabase.
+
+        [Fact]
+        public async Task CreateAsync_ShouldAddEntityToTable_And_ReturnTrue()
+        {
+            var repository = new CategoryRepository(_context); //Arrange delen, där jag gör en ny instans av ett lämpligt repository, och skickar in contextet som använder en InMemoryDatabase
+            var entity = new Category() { Name = "Test" }; //Och här skapar jag en ny entitet med lämpliga värden
+
+            bool result = await repository.CreateAsync(entity); //Act delen, där jag lägger till entiteten i InMemory databasen
+
+            Assert.True(result); //Assert delen, där jag kollar om man får tillbaka ett true värde (som man bör få om entiteten kunde läggas till i databasen)
+        }
+    }
+}
