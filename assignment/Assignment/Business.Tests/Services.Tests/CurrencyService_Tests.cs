a3bf821c0f6af22afde9aa3e8f06d0158b70ecde
Dyssling <olle.lundgarden@hotmail.com>
2024-02-11 19:21:33 +0100

    CurrencyService finished with tests.

--- assignment/Assignment/Business.Tests/Services.Tests/CurrencyService_Tests.cs
+++ assignment/Assignment/Business.Tests/Services.Tests/CurrencyService_Tests.cs
@@ -23,5 +23,80 @@ namespace Business.Tests.Services.Tests
 
             Assert.True(result); //Assert delen, där jag kollar om man får tillbaka true, vilket man bör få om allt gick som tänkt
         }
+
+        [Fact]
+        public async Task GetOneCurrencyAsync_ShouldGetOneCurrency_And_ReturnCurrencyDto()
+        {
+            var service = new CurrencyService(_context); //Arrange delen
+            CurrencyDto currency = new CurrencyDto()
+            {
+                ISOCode = "TestISO",
+                Name = "TestName"
+            };
+            await service.CreateCurrencyAsync(currency); //Lägger till den skapade entiteten i databasen
+
+            var result = await service.GetOneCurrencyAsync("TestISO"); //Act delen, där jag hämtar en entitet efter ISOCode
+            var nullResult = await service.GetOneCurrencyAsync(""); //Här testar jag även en ISOCode som inte bör hittas
+
+            Assert.Equal("TestISO", result.ISOCode); //Här kollar jag så att ISOCode stämmer, och på så vis vet jag att entiteten har hämtats
+            Assert.Null(nullResult); //Jag kollar även att det blir null på nullresult
+        }
+
+        [Fact]
+        public async Task GetAllCurrenciesAsync_ShouldGetAllCurrencies_And_ReturnIEnumerable()
+        {
+            var service = new CurrencyService(_context); //Arrange delen
+            CurrencyDto currency = new CurrencyDto()
+            {
+                ISOCode = "TestISO",
+                Name = "TestName"
+            };
+            await service.CreateCurrencyAsync(currency);
+
+            var result = await service.GetAllCurrenciesAsync(); //Act delen
+
+            Assert.Single(result); //Jag kollar om listan innehåller ett element
+            Assert.Equal("TestISO", result.First().ISOCode); //Och även om ISOCode stämmer överens
+        }
+
+        [Fact]
+        public async Task UpdateCurrencyAsync_ShouldUpdateCurrency_And_ReturnTrue()
+        {
+            var service = new CurrencyService(_context); //Arrange delen
+            CurrencyDto currency = new CurrencyDto()
+            {
+                ISOCode = "TestISO",
+                Name = "TestName"
+            };
+            await service.CreateCurrencyAsync(currency);
+            currency.Name = "NewTestName"; //Här ändrar jag namnet
+
+            var result = await service.UpdateCurrencyAsync("TestISO", currency); //Act delen, där jag uppdaterar den gamla entiteten med den nya infon
+            var getResult = await service.GetOneCurrencyAsync("TestISO"); //Jag hämtar även entiteten
+
+            Assert.True(result); //Assert delen, där jag kollar om man får tillbaka true, vilket man bör få om allt gick som tänkt
+            Assert.Equal("NewTestName", getResult.Name); //Här bör jag få det nya värdet
+        }
+
+        [Fact]
+        public async Task DeleteCurrencyAsync_ShouldDeleteCurrency_And_ReturnTrue()
+        {
+            var service = new CurrencyService(_context); //Arrange delen
+
+            CurrencyDto currency = new CurrencyDto()
+            {
+                ISOCode = "TestISO",
+                Name = "TestName"
+            };
+            await service.CreateCurrencyAsync(currency);
+
+            var result = await service.DeleteCurrencyAsync("TestISO"); //Act delen, där jag tar bort entiteten med den angivna ISOCoden
+            var falseResult = await service.DeleteCurrencyAsync("TestISO"); //Den bör returnera false, eftersom denna ISOCode inte finns längre
+            var allCurrencies = await service.GetAllCurrenciesAsync(); //Jag vill även se vilka entiteter som nu finns
+
+            Assert.True(result); //Assert delen, där jag kollar om man får tillbaka ett true värde
+            Assert.False(falseResult); //Och ett false värde på den som inte kunde hittas
+            Assert.Empty(allCurrencies); //Listan med entiteter bör nu vara tom
+        }
     }
 }
--- assignment/Assignment/Business/Services/CurrencyService.cs
+++ assignment/Assignment/Business/Services/CurrencyService.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Contexts;
 using Infrastructure.Entities;
 using Infrastructure.Repositories;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
 
 namespace Business.Services
@@ -36,6 +37,105 @@ namespace Business.Services
                 Debug.WriteLine(ex.Message);
             }
             return false; //Om metoden inte lyckas, eller om en kategori redan finns så returneras ett false värde
+        }
+
+        public async Task<CurrencyDto> GetOneCurrencyAsync(string isoCode)
+        {
+            try
+            {
+                var entity = await _repository.GetOneAsync(x => x.ISOCode == isoCode); //Jag söker efter entiteten med den angivna ISOCode
+
+                if (entity != null)
+                {
+                    CurrencyDto currency = new CurrencyDto()
+                    {
+                        ISOCode = entity.ISOCode,
+                        Name = entity.Name
+                    };
+
+                    return currency;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null!; //Om ingen entitet hittas, eller om något gick snett så returneras ett null värde
+        }
+
+        public async Task<IEnumerable<CurrencyDto>> GetAllCurrenciesAsync()
+        {
+            try
+            {
+                var entityList = await _repository.GetAllAsync(); //Jag hämtar listan med entiteter
+
+                if (!entityList.IsNullOrEmpty()) //Kollar om det finns något innehåll i listan
+                {
+                    var dtoList = new List<CurrencyDto>();//Sedan skapar jag en lista där Dto varianterna kommer lagras
+
+                    foreach (CurrencyEntity entity in entityList)
+                    {
+                        var dto = new CurrencyDto()
+                        {
+                            ISOCode = entity.ISOCode,
+                            Name = entity.Name
+                        };
+
+                        dtoList.Add(dto); //Slutligen läggs Dton till i Dto listan
+                    }
+
+                    return dtoList;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return new List<CurrencyDto>(); //Om listan är tom, eller om något gick snett, så får man tillbaka en tom lista.
+        }
+
+        public async Task<bool> UpdateCurrencyAsync(string isoCode, CurrencyDto currency)
+        {
+            try
+            {
+                var getEntity = await _repository.GetOneAsync(x => x.ISOCode == isoCode);
+
+                if (getEntity != null)
+                {
+                    var currencyEntity = new CurrencyEntity() //Dto omvandlas till en entitet
+                    {
+                        ISOCode = getEntity.ISOCode,
+                        Name = currency.Name,
+                        Orders = getEntity.Orders
+                    };
+
+                    var updateResult = await _repository.UpdateAsync((x => x.ISOCode == isoCode), currencyEntity); //Entiteten med den angivna ISOCoden ersätts med den nya entiteten
+
+                    return updateResult; //Om entiteten hittades så returneras true, annars false
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return false; //Om något gick snett så returneras false
+        }
+
+        public async Task<bool> DeleteCurrencyAsync(string isoCode)
+        {
+            try
+            {
+                var result = await _repository.DeleteAsync(x => x.ISOCode == isoCode);
+
+                return result; //Om entiteten kunde hittas så returneras true, annars false
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false; //Och om något gick snett så returneras false
         }
     }
 }
