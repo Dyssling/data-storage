8c30acb56725818f3237f979c2a4de5072b57f25
Dyssling <olle.lundgarden@hotmail.com>
2024-02-11 15:45:07 +0100

    CreateReviewAsync created with tests.

---
+++ assignment/Assignment/Business.Tests/Services.Tests/ReviewService_Tests.cs
@@ -0,0 +1,29 @@
+﻿using Business.Dtos;
+using Business.Services;
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Tests.Services.Tests
+{
+    public class ReviewService_Tests
+    {
+        private readonly ProductsDataContext _context = new ProductsDataContext(new DbContextOptionsBuilder<ProductsDataContext>().UseInMemoryDatabase($"{Guid.NewGuid()}").Options);
+
+        [Fact]
+        public async Task CreateReviewAsync_ShouldAddReviewToDatabase_And_ReturnTrue()
+        {
+            var service = new ReviewService(_context); //Arrange delen, där jag skapar en instans av min service
+            ReviewDto review = new ReviewDto()
+            {
+                CustomerId = 1,
+                Rating = 5,
+                Title = "TestTitle",
+                Content = "TestContent"
+            };
+
+            bool result = await service.CreateReviewAsync(review); //Act delen, där jag skickar in den skapade modellen i Create metoden
+
+            Assert.True(result); //Assert delen, där jag kollar om man får tillbaka true, vilket man bör få om allt gick som tänkt
+        }
+    }
+}
--- assignment/Assignment/Business/Services/ReviewService.cs
+++ assignment/Assignment/Business/Services/ReviewService.cs
@@ -1,6 +1,43 @@
-﻿namespace Business.Services
+﻿using Business.Dtos;
+using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Infrastructure.Repositories;
+using System.Diagnostics;
+
+namespace Business.Services
 {
     public class ReviewService
     {
+        private readonly ProductsDataContext _context;
+        private ReviewRepository _repository;
+
+        public ReviewService(ProductsDataContext context)
+        {
+            _context = context;
+            _repository = new ReviewRepository(_context);
+        }
+
+        public async Task<bool> CreateReviewAsync(ReviewDto review)
+        {
+            try
+            {
+                Review reviewEntity = new Review() //Dto omvandlas till en entitet
+                {
+                    CustomerId = review.CustomerId,
+                    Rating = review.Rating,
+                    Title = review.Title,
+                    Content = review.Content
+                };
+
+                var createResult = await _repository.CreateAsync(reviewEntity); //Sedan läggs entiteten till i databasen
+                return createResult; //Om den lyckades så får man true, annars false
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false; //Om metoden inte lyckas, eller om en kategori redan finns så returneras ett false värde
+        }
     }
 }
