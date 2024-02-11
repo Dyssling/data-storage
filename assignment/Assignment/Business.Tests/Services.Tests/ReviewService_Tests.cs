bddd6007003fed6aff679ed7d7f4a351627590e9
Dyssling <olle.lundgarden@hotmail.com>
2024-02-11 16:20:28 +0100

    ReviewService and tests finished.

--- assignment/Assignment/Business.Tests/Services.Tests/ReviewService_Tests.cs
+++ assignment/Assignment/Business.Tests/Services.Tests/ReviewService_Tests.cs
@@ -39,11 +39,86 @@ namespace Business.Tests.Services.Tests
             };
             await service.CreateReviewAsync(review); //Lägger till den skapade entiteten i databasen
 
-            var result = await service.GetOneReviewAsync(1); //Act delen, där jag hämtar en entitet efter CustomerId
-            var nullResult = await service.GetOneReviewAsync(2); //Här testar jag även ett CustomerId som inte bör hittas
+            var result = await service.GetOneReviewAsync(1); //Act delen, där jag hämtar en entitet efter Id
+            var nullResult = await service.GetOneReviewAsync(2); //Här testar jag även ett Id som inte bör hittas
 
-            Assert.Equal(1, result.CustomerId); //Här kollar jag så att värdena stämmer, och på så vis vet jag att reviewn har hämtats
+            Assert.Equal("TestTitle", result.Title); //Här kollar jag så att värdena stämmer, och på så vis vet jag att reviewn har hämtats
             Assert.Null(nullResult); //Jag kollar även att det blir null på nullresult
+        }
+
+        [Fact]
+        public async Task GetAllReviewsAsync_ShouldGetAllReviews_And_ReturnIEnumerable()
+        {
+            var service = new ReviewService(_context); //Arrange delen
+            ReviewDto review = new ReviewDto()
+            {
+                CustomerId = 1,
+                Rating = 5,
+                Title = "TestTitle",
+                Content = "TestContent"
+            };
+            await service.CreateReviewAsync(review);
+
+            var result = await service.GetAllReviewsAsync(); //Act delen
+
+            Assert.Single(result); //Jag kollar om listan innehåller ett element
+            Assert.Equal(1, result.First().CustomerId); //Och även om CustomerId stämmer överens
+        }
+
+        [Fact]
+        public async Task UpdateReviewAsync_ShouldUpdateReview_And_ReturnTrue()
+        {
+            var service = new ReviewService(_context); //Arrange delen
+            ReviewDto review = new ReviewDto()
+            {
+                CustomerId = 1,
+                Rating = 5,
+                Title = "TestTitle",
+                Content = "TestContent"
+            };
+            await service.CreateReviewAsync(review);
+            review.Title = "NewTitle"; //Här ändrar jag titeln
+
+            var result = await service.UpdateReviewAsync(1, review); //Act delen, där jag uppdaterar den gamla entiteten med den nya infon
+            var getResult = await service.GetOneReviewAsync(1); //Hämtar den nya entiteten
+            var resultTitle = getResult.Title; //Och dess titel
+
+            Assert.True(result); //Assert delen, där jag kollar om man får tillbaka true, vilket man bör få om allt gick som tänkt
+            Assert.Equal("NewTitle", resultTitle); //Och jag kollar så att titeln har uppdaterats
+        }
+
+        [Fact]
+        public async Task DeleteReviewAsync_ShouldDeleteReview_And_ReturnTrue()
+        {
+            var service = new ReviewService(_context); //Arrange delen
+            var productService = new ProductService(_context); //Jag skapar även en ProductService instans för att jag vill se så att relationen mellan dessa fungerar som den ska
+            ReviewDto review = new ReviewDto()
+            {
+                CustomerId = 1,
+                Rating = 5,
+                Title = "TestTitle",
+                Content = "TestContent"
+            };
+            await service.CreateReviewAsync(review);
+
+            ProductDto product = new ProductDto() //Här skapar jag en produkt som ska använda sig av samma kategori som nyss skapades, plus en ny kategori som också kommer skapas i CreateProduct metoden
+            {
+                ArticleNumber = "TestArticleNumber",
+                Name = "TestName",
+                Price = 10,
+                Categories = new List<string>() { "TestCategory" },
+                Images = new List<string>() { "TestURL" },
+                Reviews = new List<int>() { 1 }
+            };
+            await productService.CreateProductAsync(product);
+
+            var result = await service.DeleteReviewAsync(1); //Act delen, där jag tar bort entiteten med det angivna Id't
+            var falseResult = await service.DeleteReviewAsync(2); //Den bör returnera false, eftersom detta Id inte finns
+            var allReviews = await service.GetAllReviewsAsync(); //Jag vill även se hur vilka reviews
+
+            Assert.True(result); //Assert delen, där jag kollar om man får tillbaka ett true värde
+            Assert.False(falseResult); //Och ett false värde på den som inte kunde hittas
+            Assert.Empty(allReviews); //Och review listan bör vara tom
         }
     }
 }
--- assignment/Assignment/Business/Services/ReviewService.cs
+++ assignment/Assignment/Business/Services/ReviewService.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Contexts;
 using Infrastructure.Entities;
 using Infrastructure.Repositories;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
 
 namespace Business.Services
@@ -40,11 +41,11 @@ namespace Business.Services
             return false; //Om metoden inte lyckas, eller om en kategori redan finns så returneras ett false värde
         }
 
-        public async Task<ReviewDto> GetOneReviewAsync(int customerId) //Metoden kommer returnera den första recensionen av en kund, men en kund kan ju skriva flera stycken dock
+        public async Task<ReviewDto> GetOneReviewAsync(int id) //Här behöver man Id't på reviewn
         {
             try
             {
-                var entity = await _repository.GetOneAsync(x => x.CustomerId == customerId); //Jag söker efter entiteten med det angivna IDt
+                var entity = await _repository.GetOneAsync(x => x.Id == id); //Jag söker efter entiteten med det angivna IDt
 
                 if (entity != null)
                 {
@@ -64,6 +65,83 @@ namespace Business.Services
                 Debug.WriteLine(ex.Message);
             }
             return null!; //Om ingen entitet hittas, eller om något gick snett så returneras ett null värde
+        }
+
+        public async Task<IEnumerable<ReviewDto>> GetAllReviewsAsync()
+        {
+            try
+            {
+                var entityList = await _repository.GetAllAsync(); //Jag hämtar listan med entiteter
+
+                if (!entityList.IsNullOrEmpty()) //Kollar om det finns något innehåll i listan
+                {
+                    var dtoList = new List<ReviewDto>();//Sedan skapar jag en lista där Dto varianterna kommer lagras
+
+                    foreach (Review entity in entityList)
+                    {
+                        var dto = new ReviewDto()
+                        {
+                            CustomerId = entity.CustomerId,
+                            Rating = entity.Rating,
+                            Title = entity.Title,
+                            Content = entity.Content
+                        };
+
+                        dtoList.Add(dto); //Slutligen läggs Dton till i Dto listan
+                    }
+
+                    return dtoList;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return new List<ReviewDto>(); //Om listan är tom, eller om något gick snett, så får man tillbaka en tom lista.
+        }
+
+        public async Task<bool> UpdateReviewAsync(int id, ReviewDto review)
+        {
+            try
+            {
+                var getEntity = await _repository.GetOneAsync(x => x.Id == id);
+
+                var reviewEntity = new Review() //Dto omvandlas till en entitet
+                {
+                    Id = getEntity.Id,
+                    CustomerId = review.CustomerId,
+                    Rating = review.Rating,
+                    Title = review.Title,
+                    Content = review.Content,
+                    ArticleNumbers = getEntity.ArticleNumbers
+                };
+
+                var updateResult = await _repository.UpdateAsync((x => x.Id == id), reviewEntity); //Entiteten med det angivna Id't ersätts med med nya entiteten
+
+                return updateResult; //Om entiteten hittades så returneras true, annars false
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return false; //Om något gick snett så returneras false
+        }
+
+        public async Task<bool> DeleteReviewAsync(int id)
+        {
+            try
+            {
+                var result = await _repository.DeleteAsync(x => x.Id == id);
+
+                return result; //Om entiteten kunde hittas så returneras true, annars false
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false; //Och om något gick snett så returneras false
         }
     }
 }
